80f73a2ece5fba97b55f0ce19dea110fa1a3987b
MostafaAliHaider <90030448+MostafaAliHaider@users.noreply.github.com>
2022-11-30 22:15:34 +0100

    siste update

--- JS/script.js
+++ JS/script.js
@@ -5,14 +5,3 @@ hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
-/* const menu = document.getElementById("main-menu");
-
-const hamMenu = () =>{
-    if(menu.style.display == "block"){
-        menu.style.display = "none";
-    }
-    else{
-        menu.style.display = "block";
-    }
-}
- */
